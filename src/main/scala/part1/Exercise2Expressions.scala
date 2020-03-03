08e66937dab8e380197511057c38f833c14c8d13
James Palmer <james.palmer@itv.com>
2020-03-03 10:49:45 +0000

    Intro - expressions

--- src/main/scala/part1/Exercise2Expressions.scala
+++ src/main/scala/part1/Exercise2Expressions.scala
@@ -1,31 +1,56 @@
 package part1
 
+import scala.annotation.tailrec
+
 object Exercise2Expressions {
   def greet(name: String): Unit = {
-    ???
+    println(s"Hello ${name}!")
   }
 
   def greeting(name: String): String = {
-    ???
+    s"Hello ${name}!"
   }
 
   // Hint - You can reverse a string with `string.reverse`
   def palindrome(str: String): Boolean = {
-    ???
+    str == str.reverse
   }
 
   def factorial(n: Int): Int = {
-    ???
+    if (n <= 1) 1
+    else n * factorial(n -1)
   }
+
+  // to prevent stack overflow
+  def tailRecFactorial(n: Int): Int =
+  {
+    @tailrec
+    def factorialAcc(accum: Int, n: Int): Int =
+    {
+      if (n <= 1)
+        accum
+      else
+        factorialAcc(n * accum, n - 1)
+    }
+    factorialAcc(1, n)
+  }
+
 
   def main(args: Array[String]): Unit = {
     println("greet")
-    // println(greet("Earthlings"))
+     println(greet("Earthlings"))
+
+    println("greeting")
+    println(greeting("Earthlings"))
 
     println("palindrome")
-    // println(palindrome("taco"))
+     println(palindrome("abcba"))
+     println(palindrome("taco"))
 
     println("factorial")
-    // println(factorial(10))
+     println(factorial(10))
+
+    println("tailRecfactorial")
+    println(factorial(10))
   }
 }
--- src/test/scala/part1/Exercise2ExpressionsSpec.scala
+++ src/test/scala/part1/Exercise2ExpressionsSpec.scala
@@ -7,20 +7,26 @@ class Exercise2ExpressionsSpec extends AnyFlatSpec with Matchers {
   import Exercise2Expressions._
 
   "greeting" should "be nice and friendly" in {
-    pending
-    // greeting("World") should equal("Hello World!")
-    // greeting("Dave") should equal("Hello Dave!")
+//    pending
+     greeting("World") should equal("Hello World!")
+     greeting("Dave") should equal("Hello Dave!")
   }
 
   "palindrome" should "seek the tastiest mexican food felines" in {
-    pending
-    // palindrome("tacocat") should equal(true)
-    // palindrome("tacobat") should equal(false)
+//    pending
+     palindrome("tacocat") should equal(true)
+     palindrome("tacobat") should equal(false)
   }
 
   "factorial" should "multiply some numbers and stuff" in {
-    pending
-    // factorial(3) should equal (3 * 2 * 1)
-    // factorial(5) should equal (5 * 4 * 3 * 2 * 1)
+//    pending
+     factorial(3) should equal (3 * 2 * 1)
+     factorial(5) should equal (5 * 4 * 3 * 2 * 1)
+  }
+
+  "tailRecfactorial" should "multiply some numbers and stuff" in {
+    //    pending
+    tailRecFactorial(3) should equal (3 * 2 * 1)
+    tailRecFactorial(5) should equal (5 * 4 * 3 * 2 * 1)
   }
 }
