e023f2a24445c9cdb74f712e4253d64e39624bd1
James Palmer <james.palmer@itv.com>
2020-03-03 12:19:32 +0000

    ADTs

--- src/main/scala/part2/Exercise9ColorAndShape.scala
+++ src/main/scala/part2/Exercise9ColorAndShape.scala
@@ -2,28 +2,64 @@ package part2
 
 // ----------------------------------------------
 
+/*
+scala3 is bringing out Enum syntax :)
+ */
+
 // Write a definition for Color here!
 
+case class Color(red: Double, green: Double, blue: Double)
+
 // Write definitions for Shape, Circle, and Rect here!
+
+/*
+Sealed -- not only are circle and rectangles shapes, they are the only shapes
+ */
+
+// structural recursion
+object Canvas {
+  def draw(shape: Shape): Unit = {
+    shape match {
+      case Circle(radius, color) => ???
+      case Rect(width, height, color) => ???
+    }
+  }
+}
+
+sealed trait Shape {
+  def perimeter: Double
+  def area: Double
+  def color: Color
+}
+
+case class Circle(radius: Double, color: Color) extends Shape {
+  override def perimeter: Double = 2 * math.Pi * radius
+  override def area: Double = math.Pi * (radius * radius)
+}
+case class Rect(width: Double, height: Double, color: Color) extends Shape {
+  override def perimeter: Double = 2 * (width + height)
+  override def area: Double = width * height
+}
 
 // ----------------------------------------------
 
 object Exercise9ColorAndShape {
   def main(args: Array[String]): Unit = {
     println("color")
-    // println(Color(1, 1, 0))
-    // println(Color(1, 0, 1))
+     println(Color(1, 1, 0))
+     println(Color(1, 0, 1))
 
     println("circle")
-    // val circle = Circle(30, Color(1, 1, 0))
-    // println(circle)
-    // println(circle.area)
-    // println(circle.perimeter)
+     val circle = Circle(30, Color(1, 1, 0))
+     println(circle)
+     println(circle.area)
+     println(circle.perimeter)
+     println(circle.color)
 
     println("rect")
-    // val rect = Rect(50, 30, Color(1, 0, 1))
-    // println(rect)
-    // println(rect.area)
-    // println(rect.perimeter)
+     val rect = Rect(50, 30, Color(1, 0, 1))
+     println(rect)
+     println(rect.area)
+     println(rect.perimeter)
   }
 }
--- src/test/scala/part2/Exercise9ColorAndShapeSpec.scala
+++ src/test/scala/part2/Exercise9ColorAndShapeSpec.scala
@@ -4,24 +4,24 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Exercise9ColorAndShapeSpec extends AnyFlatSpec with Matchers {
-  // val shape1: Shape = Circle(10, Color(1, 1, 0))
-  // val shape2: Shape = Rect(3, 5, Color(1, 0, 1))
+   val shape1: Shape = Circle(10, Color(1, 1, 0))
+   val shape2: Shape = Rect(3, 5, Color(1, 0, 1))
 
   "area" should "calculate the area of a shape" in {
-    pending
-    // shape1.area should equal(math.Pi * 10 * 10)
-    // shape2.area should equal(3 * 5)
+//    pending
+     shape1.area should equal(math.Pi * 10 * 10)
+     shape2.area should equal(3 * 5)
   }
 
   "perimeter" should "calculate the perimeter of a shape" in {
-    pending
-    // shape1.perimeter should equal(2 * math.Pi * 10)
-    // shape2.perimeter should equal(3 * 2 + 5 * 2)
+//    pending
+     shape1.perimeter should equal(2 * math.Pi * 10)
+     shape2.perimeter should equal(3 * 2 + 5 * 2)
   }
 
   "every shape" should "have a color" in {
-    pending
-    // shape1.color should equal(Color(1, 1, 0))
-    // shape2.color should equal(Color(1, 0, 1))
+//    pending
+     shape1.color should equal(Color(1, 1, 0))
+     shape2.color should equal(Color(1, 0, 1))
   }
 }
