7aec468f87d25e51c00db2ef513c25596f9f8c53
James Palmer <james.palmer@itv.com>
2020-03-04 14:12:58 +0000

    FoldLeft

--- src/main/scala/part3/Exercise17FoldLeftAndRight.scala
+++ src/main/scala/part3/Exercise17FoldLeftAndRight.scala
@@ -4,15 +4,16 @@ import films.Film
 
 object Exercise17FoldLeftAndRight {
   def totalImdbRating(films: List[Film]): Double = {
-    ???
+    films.map(_.imdbRating).sum
   }
 
   def averageImdbRating(films: List[Film]): Double = {
-    ???
+    totalImdbRating(films) / films.length
   }
 
   def reverseUsingFold[A](items: List[A]): List[A] = {
-    ???
+     // items.reverse
+    items.foldLeft(List.empty[A])((a, b) => b :: a )
   }
 
   def filterUsingFold[A](items: List[A], pred: A => Boolean): List[A] = {
@@ -23,19 +24,19 @@ object Exercise17FoldLeftAndRight {
     import films.TestData._
 
     println("totalImdbRating")
-    // println(totalImdbRating(List(memento, outlawJoseyWales, thomasCrownAffair)))
-    // println(totalImdbRating(Nil))
+     println(totalImdbRating(List(memento, outlawJoseyWales, thomasCrownAffair)))
+     println(totalImdbRating(Nil))
 
     println("averageImdbRating")
-    // println(averageImdbRating(List(memento, outlawJoseyWales, thomasCrownAffair)))
-    // println(averageImdbRating(Nil))
+     println(averageImdbRating(List(memento, outlawJoseyWales, thomasCrownAffair)))
+     println(averageImdbRating(Nil))
 
     println("reverseUsingFold")
-    // println(reverseUsingFold(List(1, 2, 3)))
-    // println(reverseUsingFold(Nil))
+     println(reverseUsingFold(List(1, 2, 3)))
+     println(reverseUsingFold(Nil))
 
     println("filterUsingFold")
-    // println(filterUsingFold(List(1, 2, 3, 4, 5), (n: Int) => n % 2 == 0))
-    // println(filterUsingFold(Nil, (n: Int) => n % 2 == 0))
+     println(filterUsingFold(List(1, 2, 3, 4, 5), (n: Int) => n % 2 == 0))
+     println(filterUsingFold(Nil, (n: Int) => n % 2 == 0))
   }
 }
