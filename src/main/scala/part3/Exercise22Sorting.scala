d56503c49365e6607eabfa9e9048adda4f5edaa1
James Palmer <james.palmer@itv.com>
2020-03-06 14:18:46 +0000

    Sorting

--- src/main/scala/part3/Exercise22Sorting.scala
+++ src/main/scala/part3/Exercise22Sorting.scala
@@ -4,51 +4,55 @@ import films.{Director, Film}
 
 object Exercise22Sorting {
   def filmsSortedByImdb(films: List[Film]): List[Film] = {
-    ???
+    films.sortBy(_.imdbRating)
   }
 
   def filmsByDirectorSortedByImdb(director: Director): List[Film] = {
-    ???
+    filmsSortedByImdb(director.films)
   }
 
   def filmsByAllDirectorsSortedByImdb(directors: List[Director]): List[Film] = {
-    ???
+    filmsSortedByImdb(directors.flatMap(_.films))
   }
 
   def filmsByAllDirectorsSortedByDirectorNameThenImdb(directors: List[Director]): List[Film] = {
-    ???
+    directors
+      .map(d => d.copy(films = filmsSortedByImdb(d.films)))
+      .sortBy(f => f.name)
+      .flatMap(d => d.films)
   }
 
   def averageImdbRating(films: List[Film]): Double = {
-    ???
+    films.foldLeft(0.0)(_ + _.imdbRating) / films.length
   }
 
   def averageImdbRatingAcrossDirectors(directors: List[Director]): Double = {
-    ???
+    val films = directors.flatMap(_.films)
+    averageImdbRating(films)
   }
 
   def main(args:Array[String]): Unit = {
     import films.TestData._
 
     println("filmsSortedByImdb")
-    // println(filmsSortedByImdb(mcTiernan.films))
-    // println(filmsSortedByImdb(eastwood.films))
+     println(filmsSortedByImdb(mcTiernan.films))
+     println(filmsSortedByImdb(eastwood.films))
 
     println("filmsByDirectorSortedByImdb")
-    // println(filmsByDirectorSortedByImdb(mcTiernan))
-    // println(filmsByDirectorSortedByImdb(eastwood))
+     println(filmsByDirectorSortedByImdb(mcTiernan))
+     println(filmsByDirectorSortedByImdb(eastwood))
 
     println("filmsByAllDirectorsSortedByImdb")
-    // println(filmsByAllDirectorsSortedByImdb(List(nolan, mcTiernan)))
+     println(filmsByAllDirectorsSortedByImdb(List(nolan, mcTiernan)))
 
     println("filmsByAllDirectorsSortedByDirectorNameThenImdb")
-    // println(filmsByAllDirectorsSortedByDirectorNameThenImdb(List(nolan, mcTiernan)))
+     println(filmsByAllDirectorsSortedByDirectorNameThenImdb(List(nolan, mcTiernan)))
 
     println("averageImdbRating")
-    // println(averageImdbRating(nolan.films))
-    // println(averageImdbRating(someGuy.films))
+     println(averageImdbRating(nolan.films))
+     println(averageImdbRating(someGuy.films))
 
     println("averageImdbRatingAcrossDirectors")
-    // println(averageImdbRatingAcrossDirectors(directors))
+     println(averageImdbRatingAcrossDirectors(directors))
   }
 }
--- src/test/scala/part3/Exercise22SortingSpec.scala
+++ src/test/scala/part3/Exercise22SortingSpec.scala
@@ -8,51 +8,51 @@ class Exercise22SortingSpec extends AnyFlatSpec with Matchers {
   import films.TestData._
 
   "filmsSortedByImdb" should "do the right thing" in {
-    pending
-    // filmsSortedByImdb(nolan.films) should equal(List(memento, inception, darkKnight))
-    // filmsSortedByImdb(eastwood.films) should equal(List(invictus, highPlainsDrifter, outlawJoseyWales, granTorino, unforgiven))
+//    pending
+     filmsSortedByImdb(nolan.films) should equal(List(memento, inception, darkKnight))
+     filmsSortedByImdb(eastwood.films) should equal(List(invictus, highPlainsDrifter, outlawJoseyWales, granTorino, unforgiven))
   }
 
   "filmsByDirectorSortedByImdb" should "do the right thing" in {
-    pending
-    // filmsByDirectorSortedByImdb(nolan) should equal(List(memento, inception, darkKnight))
-    // filmsByDirectorSortedByImdb(eastwood) should equal(List(invictus, highPlainsDrifter, outlawJoseyWales, granTorino, unforgiven))
+//    pending
+     filmsByDirectorSortedByImdb(nolan) should equal(List(memento, inception, darkKnight))
+     filmsByDirectorSortedByImdb(eastwood) should equal(List(invictus, highPlainsDrifter, outlawJoseyWales, granTorino, unforgiven))
   }
 
   "filmsByAllDirectorsSortedByImdb" should "do the right thing" in {
-    pending
-    // filmsByAllDirectorsSortedByImdb(List(nolan, mcTiernan)) should equal(List(
-    //   thomasCrownAffair,
-    //   huntForRedOctober,
-    //   predator,
-    //   dieHard,
-    //   memento,
-    //   inception,
-    //   darkKnight,
-    // ))
+//    pending
+     filmsByAllDirectorsSortedByImdb(List(nolan, mcTiernan)) should equal(List(
+       thomasCrownAffair,
+       huntForRedOctober,
+       predator,
+       dieHard,
+       memento,
+       inception,
+       darkKnight,
+     ))
   }
 
   "filmsByAllDirectorsSortedByDirectorNameThenImdb" should "do the right thing" in {
-    pending
-    // filmsByAllDirectorsSortedByDirectorNameThenImdb(List(nolan, mcTiernan)) should equal(List(
-    //   memento,
-    //   inception,
-    //   darkKnight,
-    //   thomasCrownAffair,
-    //   huntForRedOctober,
-    //   predator,
-    //   dieHard,
-    // ))
+//    pending
+     filmsByAllDirectorsSortedByDirectorNameThenImdb(List(nolan, mcTiernan)) should equal(List(
+       memento,
+       inception,
+       darkKnight,
+       thomasCrownAffair,
+       huntForRedOctober,
+       predator,
+       dieHard,
+     ))
   }
 
   "averageImdbRating" should "do the right thing" in {
-    pending
-    // averageImdbRating(nolan.films) should equal(8.766 +- 0.001)
-    // averageImdbRating(someGuy.films).isNaN should be(true)
+//    pending
+     averageImdbRating(nolan.films) should equal(8.766 +- 0.001)
+     averageImdbRating(someGuy.films).isNaN should be(true)
   }
 
   "averageImdbRatingAcrossDirectors" should "do the right thing" in {
-    pending
-    // averageImdbRatingAcrossDirectors(directors) should equal(8.033 +- 0.001)
+//    pending
+     averageImdbRatingAcrossDirectors(directors) should equal(8.033 +- 0.001)
   }
 }
