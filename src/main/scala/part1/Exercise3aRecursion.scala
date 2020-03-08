102dd0da43f92641a0ef9fc89a0c36c274f16e2c
James Palmer <james.palmer@itv.com>
2020-03-08 13:37:14 +0000

    Hello world recursion

--- src/main/scala/part1/Exercise3aRecursion.scala
+++ src/main/scala/part1/Exercise3aRecursion.scala
@@ -2,12 +2,15 @@ package part1
 
 object Exercise3aRecursion {
   def greetNTimes(name: String, n: Int): Unit = {
-    ???
+    if (n > 0) {
+      println(s"Hello $name!")
+      greetNTimes(name, n)
+    }
   }
 
   def main(args: Array[String]): Unit = {
     println("greetNTimes")
-    // println(greetNTimes("world", 5))
-    // println(greetNTimes("nope", 0))
+     println(greetNTimes("world", 5))
+     println(greetNTimes("nope", 0))
   }
 }
